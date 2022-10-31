8dc37e3d08a6580a846c5dc7241d9feeeb298a8e
venkat <venkymec1922@gmail.com>
2022-10-31 16:32:16 +0530

    sample branch demo

--- 02-variables/var.tf
+++ 02-variables/var.tf
@@ -10,3 +10,40 @@ output "sample1" {
   value = "Hey, ${var.sample}"
 }
 
+# String Data type
+variable "sample1" {
+  default = "Hello World"
+}
+
+# Number data type
+variable "sample2" {
+  default = 100
+}
+
+# Boolean Data type
+variable "sample3" {
+  default = true
+}
+
+# List variable
+variable "sample4" {
+  default = [
+    "hello",
+    100,
+    true
+  ]
+}
+
+# Map variable
+variable "sample5" {
+  default = {
+    string  = "hello"
+    number  = 100
+    boolean = true
+  }
+}
+
+## Access a list value
+output "sample4" {
+  value = var.sample4[0]
+}
